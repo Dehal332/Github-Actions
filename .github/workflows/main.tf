b9e9c7ac52bb3dcca621bde6d2b5327a564224f0
Inderjit Singh Dehal <dehal.inderjit@outlook.com>
2026-03-17 18:33:11 -0400

    Add Azure backend configuration to main.tf
    Configure Azure backend for Terraform state management.

--- .github/workflows/main.tf
+++ .github/workflows/main.tf
@@ -1,4 +1,10 @@
 terraform {
+backend "azurerm" {
+    resource_group_name = "rg-statefile"
+    storage_account_name = "dehalstorage"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
